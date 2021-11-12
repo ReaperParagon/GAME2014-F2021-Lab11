64f609d2fee35ca1f571fefcb221177d4a104a2b
Robert Palermo <robert.palermo.official@gmail.com>
2021-11-12 15:18:07 -0500

    Updated Moving Platform

--- Assets/[Scripts]/MovingPlatformController.cs
+++ Assets/[Scripts]/MovingPlatformController.cs
@@ -10,14 +10,18 @@ public class MovingPlatformController : MonoBehaviour
     public float speed;
     [Range(0.1f, 10.0f)]
     public float distance;
+    [Range(0.05f, 0.1f)]
+    public float distanceOffset;
     public bool isLooping;
 
+    private bool movingIsActive;
     private Vector2 startingPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         startingPosition = transform.position;
+        movingIsActive = true;
     }
 
     // Update is called once per frame
@@ -28,30 +32,27 @@ public class MovingPlatformController : MonoBehaviour
 
     private void MovePlatform()
     {
-        Vector3 displacement = transform.position - new Vector3(startingPosition.x, startingPosition.y);
-        if (displacement.sqrMagnitude >= (distance * distance) - 0.1f)  // 0.1f represents the stopping distance (how close it is to the end)
+        float pingPongValue = (movingIsActive) ? Mathf.PingPong(Time.time * speed, distance) : 0.0f;
+
+        if ((!isLooping) && (pingPongValue >= distance - distanceOffset))
         {
-            isLooping = false;
+            movingIsActive = false;
         }
 
-        if (isLooping)
+        switch (direction)
         {
-            // Looping
-            switch (direction)
-            {
-                case MovingPlatformDirection.HORIZONTAL:
-                    transform.position = new Vector2(startingPosition.x + Mathf.PingPong(Time.time * speed, distance), transform.position.y);
-                    break;
-                case MovingPlatformDirection.VERTICAL:
-                    transform.position = new Vector2(transform.position.x, startingPosition.y + Mathf.PingPong(Time.time * speed, distance));
-                    break;
-                case MovingPlatformDirection.DIAGONAL_UP:
-                    transform.position = new Vector2(startingPosition.x + Mathf.PingPong(Time.time * speed, distance), startingPosition.y + Mathf.PingPong(Time.time * speed, distance));
-                    break;
-                case MovingPlatformDirection.DIAGONAL_DOWN:
-                    transform.position = new Vector2(startingPosition.x + Mathf.PingPong(Time.time * speed, distance), startingPosition.y - Mathf.PingPong(Time.time * speed, distance));
-                    break;
-            }
+            case MovingPlatformDirection.HORIZONTAL:
+                transform.position = new Vector2(startingPosition.x + pingPongValue, transform.position.y);
+                break;
+            case MovingPlatformDirection.VERTICAL:
+                transform.position = new Vector2(transform.position.x, startingPosition.y + pingPongValue);
+                break;
+            case MovingPlatformDirection.DIAGONAL_UP:
+                transform.position = new Vector2(startingPosition.x + pingPongValue, startingPosition.y + pingPongValue);
+                break;
+            case MovingPlatformDirection.DIAGONAL_DOWN:
+                transform.position = new Vector2(startingPosition.x + pingPongValue, startingPosition.y - pingPongValue);
+                break;
         }
     }
 }
